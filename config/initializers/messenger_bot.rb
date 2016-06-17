8550d4c11de5ba8731358e01c58342a2e5a467e3
kota koyama <kota-koyama@dac.co.jp>
2016-06-17 06:20:34 +0000

    Gemfile update for heroku

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+  config.access_token = EAAYw9ZC4mz60BABzlBXsBTB45ooS6kvBngD6Wmd5tFjAuMGK84VaOR5mS8MWBpuRT87GpvSr5oYUHifUb9xnR7ZAIp5WeuVyaXPQ4OSX5EU73qkhEZCwpJVZBdakNhZC1x3SfHsATfauE2eiZB58ig3jDQOexzZCXgWwO7ncIIwSQZDZD
+  config.validation_token = hoge
+  config.secret_token = a9351193017b4d85c8c501ec01dea5cc
+end

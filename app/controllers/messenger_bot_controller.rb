e82afd555b9f8d8ae3fcf180cf93924656f4d79d
kota koyama <kota-koyama@dac.co.jp>
2016-06-20 06:06:04 +0000

    test

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,7 +1,26 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-    sender.reply({ text: "Reply: #{event['message']['text']}" })
+   sender.reply({ "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text":"#{profile_last_name} #{profile_first_name}さんこんにちは",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"OVER",
+                        "payload":"OVER"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"UNDER",
+                        "payload":"UNDER"
+                    }
+                ]
+            }
+         }
+      })
   end
 
   def delivery(event, sender)
@@ -14,4 +33,5 @@ class MessengerBotController < ActionController::Base
       #ex) process sender.reply({text: "button click event!"})
     end
   end
+ 
 end

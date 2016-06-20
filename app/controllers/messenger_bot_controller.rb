773c08bbf6fc789aaf0fca50da1237138876bd97
kota koyama <kota-koyama@dac.co.jp>
2016-06-20 07:35:40 +0000

    test

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -5,8 +5,32 @@ class MessengerBotController < ActionController::Base
     if text == "おはよう"
       sender.reply({ text: "おはよう" })
     else
-      sender.reply({text: "私はおはようしかいえません"})
+      sender.reply({ "attachment":{
+                          "type":"template",
+                          "payload":{
+                              "template_type":"button",
+                              "text":"大人の恋愛ゲームを始めますか？",
+                              "buttons":[
+                                  {
+                                      "type":"postback",
+                                      "title":"はい",
+                                      "payload":"OVER"
+                                  },
+                                  {
+                                      "type":"postback",
+                                      "title":"いいえ",
+                                      "payload":"UNDER"
+                                  }
+                              ]
+                          }
+                       }
+                    })
     end
+    
+  end
+  
+  def choice(event, sender)
+    
   end
 
   def delivery(event, sender)
@@ -15,8 +39,65 @@ class MessengerBotController < ActionController::Base
   def postback(event, sender)
     payload = event["postback"]["payload"]
     case payload
-    when :something
-      #ex) process sender.reply({text: "button click event!"})
+    when "OVER"
+      sender.reply({ "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text":"ええ、はじめましょうよ？",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"何言ってるんだよ、記念日なら昨日終わっただろ（愛人）",
+                        "payload":"1"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"やややややばい！わすれてた！",
+                        "payload":"2"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"君とであった蜂蜜公園でピクニックしよう。",
+                        "payload":"3"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"楽しみにしといて！僕が全部準備しておくから！",
+                        "payload":"4"
+                    }
+                ]
+            }
+         }
+      })
+      
+    when "UNDER"
+      sender.reply({ "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text":"ええ、はじめましょうよ？",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"はい",
+                        "payload":"OVER"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"いいえ",
+                        "payload":"UNDER"
+                    }
+                ]
+            }
+         }
+      })
+      when "3"
+        sender.reply({ text: "愛してる" })
+        
+      when"1","2","4"
+        sender.reply({ text: "別れましょう" })
+        
     end
   end
 end

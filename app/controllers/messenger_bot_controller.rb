2d0e08d81f1f732ee7a136f0dbd46a7eee4757e1
kota koyama <kota-koyama@dac.co.jp>
2016-06-22 04:32:20 +0000

    test

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -111,13 +111,67 @@ class MessengerBotController < ActionController::Base
         when "1"
           sender.reply({ text: "別れましょう" })
         when "3"
-          sender.reply({"attachment":{
+         sender.reply({"attachment":{
             "type":"image",
             "payload":{
             "url":"http://kanna77.blog.so-net.ne.jp/_images/blog/_cf4/kanna77/980-kanna-003_x1000.jpg"
                       }
                   }
          })
+         sender.reply({ "attachment":{
+                        "type":"template",
+                        "payload":{
+                            "template_type":"generic",
+                            "elements":[
+                                {
+                                    "title":"【肩こり対策】(1)腕・胸・背中のストレッチ",
+                                    "image_url":"http://imgcp.aacdn.jp/img-a/auto/auto/aa/gm/article/1/9/5/4/2/7/ude.jpg",
+                                    "subtitle":"片側の腕を後頭部で曲げる。できるだけ肘を曲げるように。肘を反対側の手で後頭部に引っ張る。後頭部で肘を後ろに押すようにする。",
+                                    "buttons":[
+                                        {
+                                            "type":"postback",
+                                            "title":"腰痛対策へ",
+                                            "payload":"stretching_hip"
+                                        },{
+                                            "type":"postback",
+                                            "title":"足のむくみ対策へ",
+                                            "payload":"stretching_leg"
+                                        },{
+                                            "type":"web_url",
+                                            "url":"http://allabout.co.jp/gm/gc/195427/",
+                                            "title":"詳細をHPで見る"
+                                        }
+                                        
+                                    ]
+                                },
+                                {
+                                    "title":"【肩こり対策】(2)背中のストレッチ",
+                                    "image_url":"http://imgcp.aacdn.jp/img-a/auto/auto/aa/gm/article/1/9/5/4/2/7/senaka.jpg",
+                                    "subtitle":"背筋を伸ばして座り、両手でシャツの襟を持つ。両肘を体の正面でつける。首の力を抜いて前へ倒し首の後ろを伸ばす。背中を丸める。この時に肘が下を向くように。",
+                                    "buttons":[
+                                        {
+                                            "type":"web_url",
+                                            "url":"http://allabout.co.jp/gm/gc/195427/",
+                                            "title":"詳細をHPで見る"
+                                        }               
+                                    ]
+                                },
+                                {
+                                    "title":"【肩こり対策】(3)胸のストレッチ",
+                                    "image_url":"http://imgcp.aacdn.jp/img-a/auto/auto/aa/gm/article/1/9/5/4/2/7/mune.jpg",
+                                    "subtitle":"背筋を伸ばして腰の後ろに手を当て両手を組む。両肘を閉じる。この時に肩が上がらないように。首の力を抜いてアゴを上に。こうすると首筋もストレッチできる。",
+                                    "buttons":[
+                                        {
+                                            "type":"web_url",
+                                            "url":"http://allabout.co.jp/gm/gc/195427/",
+                                            "title":"詳細をHPで見る"
+                                        }               
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                })
     end
   end
   

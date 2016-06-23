dc1d47fe915e48e92ae9c2d81655be11e55a6bd6
kota koyama <kota-koyama@dac.co.jp>
2016-06-23 07:28:27 +0000

    test2

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -146,7 +146,7 @@ class MessengerBotController < ActionController::Base
         when "satomi_choice"
           @userdata.girl = "さとみ"
           @userdata.satomi_point += 1
-          sender.reply({ text: "#{@userdata.girl} #{@userdata.satomi_point}" })
+          sender.reply({ text: "#{@userdata.satomi_point}" })
           @userdata.save
           
         when "haruka_choice"

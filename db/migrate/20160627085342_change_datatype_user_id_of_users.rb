2fc9ceabe4bc41fee6de4afa72b86560ca55b8d1
kota koyama <kota-koyama@dac.co.jp>
2016-06-27 10:04:04 +0000

    datafix

--- db/migrate/20160627085342_change_datatype_user_id_of_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeUserIdOfUsers < ActiveRecord::Migration
-  def change
-    change_column :users, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627085342) do
+ActiveRecord::Schema.define(version: 20160627064157) do
 
   create_table "users", force: :cascade do |t|
     t.string   "boy"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160627085342) do
     t.time     "usertime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
 end

b71a76a92354da757de8998b965f5d1b294ae4b6
kota koyama <kota-koyama@dac.co.jp>
2016-06-27 09:14:36 +0000

    dbupdate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627065156) do
+ActiveRecord::Schema.define(version: 20160627085342) do
 
   create_table "users", force: :cascade do |t|
     t.string   "boy"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160627065156) do
     t.time     "usertime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "user_id"
+    t.integer  "user_id"
   end
 
 end

c185156c5b01c76f356f45cacfcaac13e248e31c
kota koyama <kota-koyama@dac.co.jp>
2016-06-27 15:35:33 +0000

    dbupdate

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -52,7 +52,7 @@ class MessengerBotController < ActionController::Base
           else 
             @point = @users.userpoint + 1
             @users.userpoint = @point
-            @users.update(users_params)
+            @users.update(user_params)
                 sender.reply({ text: "今は言葉を返してくれる人がいないよ！「かんな！」と呼んでみて！#{@users.userpoint}"})
           
           end

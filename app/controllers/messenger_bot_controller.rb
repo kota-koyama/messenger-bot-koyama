f8ab1d9ba88d44c67e02f9f63c277d73c8eb664f
kota koyama <kota-koyama@dac.co.jp>
2016-06-28 00:57:10 +0000

    userpoint setting

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -51,7 +51,6 @@ class MessengerBotController < ActionController::Base
                       
           else 
             @users.userpoint = @users.userpoint + 1
-            @users = User.find(@users.userpoint)
             @users.update(user_params)
                 sender.reply({ text: "今は言葉を返してくれる人がいないよ！「かんな！」と呼んでみて！#{@users.userpoint}"})
           

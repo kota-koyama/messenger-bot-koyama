1b8c4fc154e4adbc5cc7ee459f3ad676771db6ea
kota koyama <kota-koyama@dac.co.jp>
2016-07-05 07:37:46 +0000

    add image

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -448,9 +448,8 @@ class MessengerBotController < ActionController::Base
                 
           elsif @users.userpoint == 5
           
-            if text == nil
-                @call = sender.reply({ text: "忙しいのかな？連絡ほしいな。。。"})
-                setTimeout(@call , 500)
+            if text == "またね！"
+                setTimeout(sender.reply({ text: "忙しいのかな？連絡ほしいな。。。"}) , 500)
             else
                 if text =="好き"|| text.include?("#{@yamasita}好き") || text.include?("#{@yamasita}すき")||text.include?("#{@yamasita}スキ")||text.include?("好き")
                 
